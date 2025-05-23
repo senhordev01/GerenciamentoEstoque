0b35465118a5be09a27477e53642ca1cfcf73376
Rogerio <contaprogramacao230@gmail.com>
2025-05-23 18:24:13 -0300

    Ultimo Backup do Rogerio

--- projeto/todos/admin.py
+++ projeto/todos/admin.py
@@ -4,11 +4,11 @@ from .models import Produto, Dados
 
 @admin.register(Produto)
 class ProdutoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'descricao', 'preco', 'wstoque', 'disponivel')
+    list_display = ('nome', 'preco', 'estoque', 'disponivel')
     list_editable = ('preco', 'estoque')
 
 @admin.register(Dados)
 class DadosAdmin(admin.ModelAdmin):
     list_display = ('nome', 'email', 'senha')
-    list_editable = ('senha', 'email')
+    list_editable = ('email', 'senha')
     search_fields = ('nome', 'email')
--- projeto/todos/models.py
+++ projeto/todos/models.py
@@ -13,7 +13,7 @@ class Produto(models.Model):
     nome = models.CharField(max_length=100)
     descricao = models.TextField()
     preco = models.DecimalField(max_digits=10, decimal_places=2)
-    wstoque = models.IntegerField()
+    estoque = models.IntegerField()
     disponivel = models.BooleanField(default=True)
     def __str__(self):
         return self.nome
