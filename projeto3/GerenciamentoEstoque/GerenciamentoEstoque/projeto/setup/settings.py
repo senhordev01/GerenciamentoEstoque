f9b4cd7d8a10e9333da7f4a59573d113e164152f
N0vaEZ <n0vaezemail@gmail.com>
2025-05-29 19:43:19 -0300

    entrar site default 2

--- projeto3/GerenciamentoEstoque/GerenciamentoEstoque/projeto/setup/settings.py
+++ projeto3/GerenciamentoEstoque/GerenciamentoEstoque/projeto/setup/settings.py
@@ -104,7 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = 'PT-BR'
 
 TIME_ZONE = 'UTC'
 
